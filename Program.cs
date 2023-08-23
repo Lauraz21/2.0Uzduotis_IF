064584eabeb07568f808dbd13f0aa9b1458379ef
Laura <Laura@DESKTOP-BESSB6E>
2023-08-23 23:09:44 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Iveskite skaiciu: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number % 2 == 0)
+{
+    Console.WriteLine("Skaicius yra lyginis");
+}
+else if (number % 5 == 0)
+{
+    Console.WriteLine("Skaicius dalijasi is 5");
+}
+else
+{
+    Console.WriteLine("Skaicius neatitinka jokiu salygu");
+}
